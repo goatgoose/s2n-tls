5d3fe826f162effed1c51661dfae18cbea06257c
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2023-05-31 16:22:30 -0400

    add fuzz test

---
+++ tests/fuzz/s2n_libcrypto_diff_prf_test.c
@@ -0,0 +1,27 @@
+/*
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+#include "s2n_test.h"
+#include "tls/s2n_prf.h"
+
+#define MAX_BLOB_SIZE 64
+
+int s2n_fuzz_test(const uint8_t *buf, size_t len)
+{
+
+    return S2N_SUCCESS;
+}
+
+S2N_FUZZ_TARGET(NULL, s2n_fuzz_test, NULL)
