246d6b7ec965e1818070b4defb445aeaba75ba01
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2023-07-18 17:02:59 -0400

    only define .init

--- crypto/s2n_hmac.c
+++ crypto/s2n_hmac.c
@@ -180,9 +180,9 @@ int s2n_hmac_hash_block_size(s2n_hmac_algorithm hmac_alg, uint16_t *block_size)
 }
 
 struct s2n_hmac_impl {
-    S2N_RESULT (*init)(struct s2n_hmac_state *hmac, s2n_hmac_algorithm alg, const void *key, uint32_t klen);
+    S2N_RESULT (*init)(struct s2n_hmac_state *hmac, s2n_hmac_algorithm alg, const void *key, uint32_t key_len);
     S2N_RESULT (*update)(struct s2n_hmac_state *hmac, const void *data, uint32_t size);
-    S2N_RESULT (*digest)(struct s2n_hmac_state *hmac, void *digest, uint32_t size);
+    S2N_RESULT (*digest)(struct s2n_hmac_state *hmac, void *out, uint32_t size);
     S2N_RESULT (*reset)(struct s2n_hmac_state *hmac);
     S2N_RESULT (*copy)(struct s2n_hmac_state *hmac_to, struct s2n_hmac_state *hmac_from);
 };
@@ -312,10 +312,10 @@ static S2N_RESULT s2n_custom_hmac_copy(struct s2n_hmac_state *to, struct s2n_hma
 
 const struct s2n_hmac_impl s2n_custom_hmac_impl = {
     .init = &s2n_custom_hmac_init,
-    .update = &s2n_custom_hmac_update,
-    .digest = &s2n_custom_hmac_digest,
-    .reset = &s2n_custom_hmac_reset,
-    .copy = &s2n_custom_hmac_copy
+//    .update = &s2n_custom_hmac_update,
+//    .digest = &s2n_custom_hmac_digest,
+//    .reset = &s2n_custom_hmac_reset,
+//    .copy = &s2n_custom_hmac_copy
 };
 
 //const struct s2n_hmac_impl *s2n_hmac_get_impl()
