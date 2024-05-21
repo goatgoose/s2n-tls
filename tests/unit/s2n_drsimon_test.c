0fb6be3f348050531f27d5ec6764dba67e531645
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2024-05-21 10:36:28 -0400

    wip

--- tests/unit/s2n_drsimon_test.c
+++ tests/unit/s2n_drsimon_test.c
@@ -21,15 +21,130 @@ int main(int argc, char **argv)
 {
     BEGIN_TEST();
 
-    DEFER_CLEANUP(struct s2n_cert_chain_and_key *chain_and_key = NULL,
+    DEFER_CLEANUP(struct s2n_cert_chain_and_key *ecdsa_chain_and_key_256 = NULL,
             s2n_cert_chain_and_key_ptr_free);
-    EXPECT_SUCCESS(s2n_test_cert_chain_and_key_new(&chain_and_key,
+    EXPECT_SUCCESS(s2n_test_cert_chain_and_key_new(&ecdsa_chain_and_key_256,
+            S2N_ECDSA_P256_PKCS1_CERT_CHAIN, S2N_ECDSA_P256_PKCS1_KEY));
+
+    DEFER_CLEANUP(struct s2n_cert_chain_and_key *ecdsa_chain_and_key_384 = NULL,
+            s2n_cert_chain_and_key_ptr_free);
+    EXPECT_SUCCESS(s2n_test_cert_chain_and_key_new(&ecdsa_chain_and_key_384,
             S2N_ECDSA_P384_PKCS1_CERT_CHAIN, S2N_ECDSA_P384_PKCS1_KEY));
+
+    DEFER_CLEANUP(struct s2n_cert_chain_and_key *ecdsa_chain_and_key_512 = NULL,
+            s2n_cert_chain_and_key_ptr_free);
+    EXPECT_SUCCESS(s2n_test_cert_chain_and_key_new(&ecdsa_chain_and_key_512,
+            S2N_ECDSA_P512_CERT_CHAIN, S2N_ECDSA_P512_KEY));
+
+    DEFER_CLEANUP(struct s2n_cert_chain_and_key *rsa_chain_and_key = NULL,
+            s2n_cert_chain_and_key_ptr_free);
+    EXPECT_SUCCESS(s2n_test_cert_chain_and_key_new(&rsa_chain_and_key,
+            S2N_RSA_PSS_2048_SHA256_LEAF_CERT, S2N_RSA_PSS_2048_SHA256_LEAF_KEY));
+
+    {
+        DEFER_CLEANUP(struct s2n_config *client_config = s2n_config_new(),
+                s2n_config_ptr_free);
+        EXPECT_SUCCESS(s2n_config_set_cipher_preferences(client_config, "test_all_tls13"));
+        EXPECT_SUCCESS(s2n_config_disable_x509_verification(client_config));
+
+        struct {
+            const struct s2n_signature_scheme *signature_scheme;
+            struct s2n_cert_chain_and_key *chain_and_key;
+        } test_cases[] = {
+//            {
+//                    .signature_scheme = &s2n_ecdsa_sha256,
+//                    .chain_and_key = ecdsa_chain_and_key_256,
+//            },
+//            {
+//                    .signature_scheme = &s2n_ecdsa_sha384,
+//                    .chain_and_key = ecdsa_chain_and_key_384,
+//            },
+//            {
+//                    .signature_scheme = &s2n_ecdsa_sha512,
+//                    .chain_and_key = ecdsa_chain_and_key_512,
+//            },
+            {
+                    .signature_scheme = &s2n_rsa_pss_pss_sha256,
+                    .chain_and_key = rsa_chain_and_key,
+            }
+        };
+
+        for (size_t test_idx = 0; test_idx < s2n_array_len(test_cases); test_idx++) {
+            printf("test_idx: %d\n", test_idx);
+
+            struct s2n_cert_chain_and_key *chain_and_key = test_cases[test_idx].chain_and_key;
+            DEFER_CLEANUP(struct s2n_config *server_config = s2n_config_new(),
+                    s2n_config_ptr_free);
+            EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(server_config, chain_and_key));
+            EXPECT_SUCCESS(s2n_config_set_cipher_preferences(server_config, "test_all_tls13"));
+
+            const struct s2n_signature_scheme *sig_scheme = test_cases[test_idx].signature_scheme;
+            struct s2n_signature_preferences sig_prefs = {
+                .count = 1,
+                .signature_schemes = &sig_scheme
+            };
+            struct s2n_security_policy policy = security_policy_test_all_tls13;
+            policy.signature_preferences = &sig_prefs;
+
+            DEFER_CLEANUP(struct s2n_connection *server_conn = s2n_connection_new(S2N_SERVER),
+                    s2n_connection_ptr_free);
+            EXPECT_NOT_NULL(server_conn);
+            EXPECT_SUCCESS(s2n_connection_set_blinding(server_conn, S2N_SELF_SERVICE_BLINDING));
+            EXPECT_SUCCESS(s2n_connection_set_config(server_conn, server_config));
+            server_conn->security_policy_override = &policy;
+
+            DEFER_CLEANUP(struct s2n_connection *client_conn = s2n_connection_new(S2N_CLIENT),
+                    s2n_connection_ptr_free);
+            EXPECT_NOT_NULL(client_conn);
+            EXPECT_SUCCESS(s2n_connection_set_blinding(client_conn, S2N_SELF_SERVICE_BLINDING));
+            EXPECT_SUCCESS(s2n_connection_set_config(client_conn, client_config));
+
+            struct s2n_test_io_pair io_pair = { 0 };
+            EXPECT_SUCCESS(s2n_io_pair_init_non_blocking(&io_pair));
+            EXPECT_SUCCESS(s2n_connections_set_io_pair(client_conn, server_conn, &io_pair));
+
+            /* Skip to before the server sends the CertificateVerify message. */
+            s2n_blocked_status blocked = 0;
+            EXPECT_OK(s2n_negotiate_test_server_and_client_until_message(server_conn, client_conn, SERVER_CERT_VERIFY));
+
+            /* Overwrite the SignatureScheme field to lie about the signature/hash algorithm used
+             * to sign the signature content field.
+             */
+            EXPECT_SUCCESS(s2n_stuffer_rewrite(&server_conn->handshake.io));
+            EXPECT_SUCCESS(s2n_tls13_cert_verify_send(server_conn));
+            struct s2n_stuffer cert_verify_stuffer = server_conn->handshake.io;
+            EXPECT_SUCCESS(s2n_stuffer_rewrite(&cert_verify_stuffer));
+            const struct s2n_signature_scheme *overwrite_sig_scheme = &s2n_ecdsa_sha256;
+            EXPECT_SUCCESS(s2n_stuffer_write_uint16(&cert_verify_stuffer, overwrite_sig_scheme->iana_value));
+
+            EXPECT_SUCCESS(s2n_stuffer_wipe(&client_conn->handshake.io));
+            EXPECT_SUCCESS(s2n_stuffer_copy(&server_conn->handshake.io, &client_conn->handshake.io,
+                    s2n_stuffer_data_available(&server_conn->handshake.io)));
+
+            int ret = s2n_tls13_cert_verify_recv(client_conn);
+            if (sig_scheme == overwrite_sig_scheme) {
+                /* If the overwritten signature algorithm was the same one used to hash the struct,
+                 * verification should succeed.
+                 */
+                EXPECT_SUCCESS(ret);
+            } else {
+                /* Otherwise, verification should fail, since the hash algorithm used to hash the
+                 * ServerDHParams struct on the receive side is different from on the send side.
+                 * This causes the resulting signature to be different.
+                 */
+                EXPECT_FAILURE(ret);
+                //EXPECT_SUCCESS(ret);
+            }
+
+            EXPECT_EQUAL(server_conn->handshake_params.server_cert_sig_scheme, sig_scheme);
+            EXPECT_EQUAL(client_conn->handshake_params.server_cert_sig_scheme, overwrite_sig_scheme);
+        }
+    }
 
     {
         DEFER_CLEANUP(struct s2n_config *config = s2n_config_new(),
                 s2n_config_ptr_free);
-        EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, chain_and_key));
+        EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, ecdsa_chain_and_key_384));
         EXPECT_SUCCESS(s2n_config_set_cipher_preferences(config, "test_all_tls12"));
         EXPECT_SUCCESS(s2n_config_disable_x509_verification(config));
 
