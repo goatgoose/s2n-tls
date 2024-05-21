3882f3f9835b71e6401cf6e30d0c277cd8f6c50e
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2024-05-21 13:30:54 -0400

    wip

--- tests/unit/s2n_drsimon_test.c
+++ tests/unit/s2n_drsimon_test.c
@@ -42,10 +42,106 @@ int main(int argc, char **argv)
             S2N_RSA_PSS_2048_SHA256_LEAF_CERT, S2N_RSA_PSS_2048_SHA256_LEAF_KEY));
 
     {
+        DEFER_CLEANUP(struct s2n_config *config = s2n_config_new(),
+                s2n_config_ptr_free);
+        EXPECT_SUCCESS(s2n_config_set_cipher_preferences(config, "test_all_tls13"));
+        EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(config, ecdsa_chain_and_key_256));
+        EXPECT_SUCCESS(s2n_config_disable_x509_verification(config));
+
+        const struct s2n_signature_scheme *sig_scheme = &s2n_ecdsa_sha256;
+        struct s2n_signature_preferences sig_prefs = {
+            .count = 1,
+            .signature_schemes = &sig_scheme
+        };
+        struct s2n_security_policy policy = security_policy_test_all_tls13;
+        policy.signature_preferences = &sig_prefs;
+        config->security_policy = &policy;
+
+        const struct s2n_signature_scheme *test_signature_schemes[] = {
+//            &s2n_ecdsa_sha256,
+//            &s2n_ecdsa_sha384,
+//            &s2n_ecdsa_sha512,
+            &s2n_rsa_pss_rsae_sha256,
+        };
+
+        for (size_t sig_idx = 0; sig_idx < s2n_array_len(test_signature_schemes); sig_idx++) {
+            printf("sig_idx: %zu\n", sig_idx);
+
+            DEFER_CLEANUP(struct s2n_connection *server_conn = s2n_connection_new(S2N_SERVER),
+                    s2n_connection_ptr_free);
+            EXPECT_NOT_NULL(server_conn);
+            EXPECT_SUCCESS(s2n_connection_set_blinding(server_conn, S2N_SELF_SERVICE_BLINDING));
+            EXPECT_SUCCESS(s2n_connection_set_config(server_conn, config));
+            server_conn->security_policy_override = &policy;
+
+            DEFER_CLEANUP(struct s2n_connection *client_conn = s2n_connection_new(S2N_CLIENT),
+                    s2n_connection_ptr_free);
+            EXPECT_NOT_NULL(client_conn);
+            EXPECT_SUCCESS(s2n_connection_set_blinding(client_conn, S2N_SELF_SERVICE_BLINDING));
+            EXPECT_SUCCESS(s2n_connection_set_config(client_conn, config));
+            client_conn->security_policy_override = &policy;
+
+            struct s2n_test_io_pair io_pair = { 0 };
+            EXPECT_SUCCESS(s2n_io_pair_init_non_blocking(&io_pair));
+            EXPECT_SUCCESS(s2n_connections_set_io_pair(client_conn, server_conn, &io_pair));
+
+            /* Skip to before the server sends the CertificateVerify message. */
+            s2n_blocked_status blocked = 0;
+            EXPECT_OK(s2n_negotiate_test_server_and_client_until_message(server_conn, client_conn, SERVER_CERT_VERIFY));
+
+            /* Force the server to sign the CertificateVerify message with a different signature
+             * scheme.
+             */
+            const struct s2n_signature_scheme *signing_sig_scheme = test_signature_schemes[sig_idx];
+            server_conn->handshake_params.server_cert_sig_scheme = signing_sig_scheme;
+            EXPECT_SUCCESS(s2n_stuffer_rewrite(&server_conn->handshake.io));
+            EXPECT_SUCCESS(s2n_tls13_cert_verify_send(server_conn));
+
+            /* Overwrite the SignatureScheme field to lie about the signature scheme used to sign
+             * the CertificateVerify message.
+             */
+            struct s2n_stuffer cert_verify_stuffer = server_conn->handshake.io;
+            EXPECT_SUCCESS(s2n_stuffer_rewrite(&cert_verify_stuffer));
+            const struct s2n_signature_scheme *overwrite_sig_scheme = sig_scheme;
+            EXPECT_SUCCESS(s2n_stuffer_write_uint16(&cert_verify_stuffer, overwrite_sig_scheme->iana_value));
+
+            EXPECT_SUCCESS(s2n_stuffer_wipe(&client_conn->handshake.io));
+            EXPECT_SUCCESS(s2n_stuffer_copy(&server_conn->handshake.io, &client_conn->handshake.io,
+                    s2n_stuffer_data_available(&server_conn->handshake.io)));
+
+            int ret = s2n_tls13_cert_verify_recv(client_conn);
+            if (signing_sig_scheme == sig_scheme) {
+                /* If the written signature scheme matches the signature scheme used to sign the
+                 * CertificateVerify message, verifying the message should succeed.
+                 */
+                EXPECT_SUCCESS(ret);
+            } else {
+                /* Otherwise, a different signature scheme should be used during verification, so
+                 * verification should fail.
+                 */
+                EXPECT_FAILURE(ret);
+            }
+        }
+    }
+
+    {
         DEFER_CLEANUP(struct s2n_config *client_config = s2n_config_new(),
                 s2n_config_ptr_free);
-        EXPECT_SUCCESS(s2n_config_set_cipher_preferences(client_config, "test_all_tls13"));
         EXPECT_SUCCESS(s2n_config_disable_x509_verification(client_config));
+        EXPECT_SUCCESS(s2n_config_set_cipher_preferences(client_config, "test_all_tls13"));
+
+//        const struct s2n_signature_scheme *all_ecdsa_sig_schemes[] = {
+//            &s2n_ecdsa_sha256,
+//            &s2n_ecdsa_sha384,
+//            &s2n_ecdsa_sha512,
+//            &s2n_ecdsa_sha224,
+//        };
+//        struct s2n_signature_preferences only_ecdsa = {
+//            .count = s2n_array_len(all_ecdsa_sig_schemes),
+//            .signature_schemes = all_ecdsa_sig_schemes,
+//        };
+//        struct s2n_security_policy client_policy = security_policy_test_all_tls13;
+//        client_policy.signature_preferences = &only_ecdsa;
 
         struct {
             const struct s2n_signature_scheme *signature_scheme;
@@ -70,28 +166,25 @@ int main(int argc, char **argv)
         };
 
         for (size_t test_idx = 0; test_idx < s2n_array_len(test_cases); test_idx++) {
-            printf("test_idx: %d\n", test_idx);
-
             struct s2n_cert_chain_and_key *chain_and_key = test_cases[test_idx].chain_and_key;
             DEFER_CLEANUP(struct s2n_config *server_config = s2n_config_new(),
                     s2n_config_ptr_free);
             EXPECT_SUCCESS(s2n_config_add_cert_chain_and_key_to_store(server_config, chain_and_key));
-            EXPECT_SUCCESS(s2n_config_set_cipher_preferences(server_config, "test_all_tls13"));
 
             const struct s2n_signature_scheme *sig_scheme = test_cases[test_idx].signature_scheme;
             struct s2n_signature_preferences sig_prefs = {
                 .count = 1,
                 .signature_schemes = &sig_scheme
             };
-            struct s2n_security_policy policy = security_policy_test_all_tls13;
-            policy.signature_preferences = &sig_prefs;
+            struct s2n_security_policy server_policy = security_policy_test_all_tls13;
+            server_policy.signature_preferences = &sig_prefs;
 
             DEFER_CLEANUP(struct s2n_connection *server_conn = s2n_connection_new(S2N_SERVER),
                     s2n_connection_ptr_free);
             EXPECT_NOT_NULL(server_conn);
             EXPECT_SUCCESS(s2n_connection_set_blinding(server_conn, S2N_SELF_SERVICE_BLINDING));
             EXPECT_SUCCESS(s2n_connection_set_config(server_conn, server_config));
-            server_conn->security_policy_override = &policy;
+            server_conn->security_policy_override = &server_policy;
 
             DEFER_CLEANUP(struct s2n_connection *client_conn = s2n_connection_new(S2N_CLIENT),
                     s2n_connection_ptr_free);
@@ -132,12 +225,12 @@ int main(int argc, char **argv)
                  * ServerDHParams struct on the receive side is different from on the send side.
                  * This causes the resulting signature to be different.
                  */
-                EXPECT_FAILURE(ret);
+                //EXPECT_FAILURE(ret);
                 //EXPECT_SUCCESS(ret);
             }
 
-            EXPECT_EQUAL(server_conn->handshake_params.server_cert_sig_scheme, sig_scheme);
-            EXPECT_EQUAL(client_conn->handshake_params.server_cert_sig_scheme, overwrite_sig_scheme);
+            //EXPECT_EQUAL(server_conn->handshake_params.server_cert_sig_scheme, sig_scheme);
+            //EXPECT_EQUAL(client_conn->handshake_params.server_cert_sig_scheme, overwrite_sig_scheme);
         }
     }
 
